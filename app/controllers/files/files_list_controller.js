9d4ff0ce0c1f0378b888d34ca5363d61d972cc0b
kkaragiorgis <konstanty@karagiorgis.pl>
2015-08-31 22:18:50 +0200

    filelist: Added icons fading from right on hover Those icons are auto shown on mobile added hover effect

--- app/controllers/files/files_list_controller.js
+++ app/controllers/files/files_list_controller.js
@@ -32,19 +32,19 @@ angular.module('Handy.controllers.files.list', ['ngRoute'])
                 name: "Ziomek.jpg",
                 size: "34.2KB",
                 modified: "12-03-2012",
-                url: "http://i.ytimg.com/vi/s2fiFKZf7rk/hqdefault.jpg"
+                url: "img/1.jpg"
             },
             {
                 name: "Rosa.jpg",
                 size: "14.2KB",
                 modified: "14-12-2014",
-                url: "https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c.jpeg"
+                url: "img/2.jpg"
             },
             {
                 name: "Doge.png",
                 size: "11.2KB",
                 modified: "14-12-2014",
-                url: "http://i.stack.imgur.com/aP2dv.gif"
+                url: "img/3.png"
             }
         ];
 
